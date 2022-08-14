4cbb73ebce3056644d55bf598ad0c80d21059f03
Alelopez383 <43974872+Alelopez383@users.noreply.github.com>
2022-08-13 19:34:31 -0500

    Add files via upload

---
+++ counting_vowels_code.py
@@ -0,0 +1,49 @@
+#challenge
+
+def count_vowels(phrase):
+    count=0
+    for letter in phrase:
+        if letter in "aeiouAEIOU":
+            count += 1
+    return count
+
+phrase = 'I don´t get it. Maybe this is not for me. SOS.'
+number = count_vowels(phrase)
+
+print(number)
+
+print ( f' In the phrase: " {phrase}" hay {number} vowels.')    
+
+
+
+#only lower cases
+def count_vowels(phrase):
+    count=0
+    for letter in phrase:
+        if letter in "aeiou":
+            count += 1
+    return count
+
+phrase = 'I don´t get it. Maybe this is not for me. SOS.'
+number = count_vowels(phrase)
+
+print(number)
+
+print ( f' In the phrase: " {phrase}" hay {number} vocales.')    
+
+#only consonants
+def count_notvowels(phrase):
+    count=0
+    vowels= 'aeiouAEIOU'
+    
+    for letter in phrase:
+        if letter not in vowels:
+            count += 1
+    return count
+
+phrase = 'I don´t get it. Maybe this is not for me. SOS.'
+number = count_notvowels(phrase)
+
+print(number)
+
+print ( f' In the phrase: " {phrase}" hay {number} consonants.')    
